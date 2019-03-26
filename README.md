1c406cdc5478e09124119e19de43fba440171459
Steve <steve@tech-otaku.com>
2019-03-26 09:02:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# launch-agents-symlinks
+Creates symbolic links to `.plist` files. Jobs are unloaded before the symbolic links are created and then (re-)loaded afterwards.
+
+Source `.plist` files are in `${HOME}/Library/Mobile\Documents/com~apple~CloudDocs/LaunchAgents`
+
+Symbolic links are created in `${HOME}/Library/LaunchAgents`
---
+++ launch-agents-symlinks.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for e in ~/Library/Mobile\ Documents/com~apple~CloudDocs/LaunchAgents/*; do
+    f=$( basename "${e}" )
+	launchctl unload "${HOME}/Library/LaunchAgents/${f}" > /dev/null 2>&1
+	ln -sf "${e}" "${HOME}"/Library/LaunchAgents
+	launchctl load "${HOME}/Library/LaunchAgents/${f}"
+done
+
+launchctl list | grep steve
